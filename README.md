3012423edb50a2cd00b175f851072950d4957c68
Stefanos Chrs <stefanoschrs@hotmail.com>
2016-03-31 21:33:07 +0300

    v1.1.0 - Remove routing for images and add error handler

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Facebook-Image-Caching-Server
+# Facebook Image Caching Server
 Simple caching server for images retrieved from graph
+
+Prerequisites
+-
+* `pip`
+* `virtualenv`
+
+Installation (recommended)
+-
+* Create the env `virtualenv --no-site-packages --distribute .env && source .env/bin/activate && pip install -r requirements.txt`
+* Get a facebook access token (User token recommended since App token has some restrictions)
+* Add the token to a file so you can easily change it `echo $ACCESS_TOKEN > access_token.txt`
+
+Usage
+-
+* Start the server `python app.py`
+* Visit `/image/<page_id>`
+
+Releases
+-
+*1.1.0* - Added error handler so the images are served as static assets if they exist so they don't have to pass through the routing
+on every request
--- app.py
+++ app.py
@@ -4,33 +4,42 @@ Simple caching server for images retrieved from graph
 """
 
 import urllib
+import re
 from os import path
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 
 __author__ = "Stefanos Chrs"
 __email__ = "stefanoschrs@hotmail.com"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __status__ = "Production"
 __license__ = "MIT"
 
 
-IMAGES_DIR = './images'
-TOKEN_LOCATION = 'access_token.txt'
+IMAGES_DIR = './static/images'
+# TOKEN_LOCATION = 'access_token.txt'
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 def fetch_image(page_id):
-	urllib.urlretrieve('https://graph.facebook.com/' + page_id + '/picture?type=large& access_token=' + open(TOKEN_LOCATION, 'r').read(), IMAGES_DIR + '/' + page_id)
+	# For some reason when using the access token the user profile picture is the default
+	# urllib.urlretrieve('https://graph.facebook.com/' + page_id + '/picture?type=large& access_token=' + open(TOKEN_LOCATION, 'r').read(), IMAGES_DIR + '/' + page_id)
+	urllib.urlretrieve('https://graph.facebook.com/' + page_id + '/picture?type=large', IMAGES_DIR + '/' + page_id)
 
 @app.route('/')
 def index():
     return 'Facebook Image Caching API ' + __version__
 
-@app.route('/image/<page_id>')
-def image(page_id):
-	if not path.isfile(IMAGES_DIR+'/'+page_id):
-		fetch_image(page_id)
-	return send_file(IMAGES_DIR+'/'+page_id)
+@app.errorhandler(404)
+def page_not_found(e):
+	if '/images/' in request.url:
+		page_id = request.url.split('/')[-1]
+		p = re.compile('^[0-9]+$')
+		if p.match(page_id):
+			fetch_image(page_id)
+			return send_file(IMAGES_DIR+'/'+page_id)
+		else:
+			return 'Wrong Facebook ID'
+	return 'Page not found'
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
 	app.run(debug=False, host='0.0.0.0')
